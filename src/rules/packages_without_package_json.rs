1bb79715c3728bb9521d920bf4066f0e4de06c11
QuiiBz <tom.lienrd@gmail.com>
2023-09-18 12:09:18 +0200

    refactor: improve README & tweak issues level

--- src/rules/packages_without_package_json.rs
+++ src/rules/packages_without_package_json.rs
@@ -46,3 +46,42 @@ impl Issue for PackagesWithoutPackageJsonIssue {
         Some(self)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let issue = PackagesWithoutPackageJsonIssue::new();
+
+        assert_eq!(issue.name(), "packages-without-package-json");
+        assert_eq!(issue.level(), IssueLevel::Warning);
+    }
+
+    #[test]
+    fn single_package() {
+        let mut issue = PackagesWithoutPackageJsonIssue::new();
+        issue.add_package("test".to_string());
+
+        colored::control::set_override(false);
+        assert_eq!(
+            issue.message(),
+            "1 package doesn't have a package.json file: test"
+        );
+    }
+
+    #[test]
+    fn multiple_packages() {
+        let mut issue = PackagesWithoutPackageJsonIssue::new();
+        issue.add_package("test".to_string());
+        issue.add_package("test-2".to_string());
+        issue.add_package("test-3".to_string());
+
+        colored::control::set_override(false);
+        assert_eq!(
+            issue.message(),
+            "3 packages doesn't have a package.json file: test, test-2, test-3"
+        );
+    }
+}
